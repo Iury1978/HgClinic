9ea4e954d9a0beb78fe00e24a3319d9cd97c92b3
Iury1978 <advent777@yandex.ru>
2022-12-15 15:42:30 +0200

    remove scroll from the body  when burger is open

--- js/module/burger.js
+++ js/module/burger.js
@@ -6,12 +6,18 @@ const closeBurger = (event) => {
   const target = event.target;
   if (target.closest(".burger__close") || event.code === "Escape") {
     burger.classList.remove("burger_active");
+    window.removeEventListener("keydown", closeBurger);
+    document.body.style.cssText = ``;
   }
 };
 
 const openBurger = () => {
   burger.classList.add("burger_active");
   window.addEventListener("keydown", closeBurger);
+  document.body.style.cssText = `
+     overflow: hidden;
+      position: fixed;
+    `;
 };
 
 headerBurger.addEventListener("click", openBurger);
