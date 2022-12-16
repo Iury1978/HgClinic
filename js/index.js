65306e20381810795056bf132c8f32886ddb452f
Iury1978 <advent777@yandex.ru>
2022-12-16 17:15:15 +0200

    rewrited section about

--- js/index.js
+++ js/index.js
@@ -3,3 +3,4 @@ import "./module/hero-swiper.js";
 import "./module/doctors-swiper.js";
 import "./module/map.js";
 import "./module/modal-appointment.js";
+import "./module/about.js";
---
+++ js/module/about.js
@@ -0,0 +1,7 @@
+// const aboutBtnMore = document.querySelector(".about__btn-more");
+// const aboutDescriptionMore = document.querySelector(".about__description-more");
+// const aboutBtnMoreHide = document.querySelector(".about__btn-more-hide");
+
+// aboutBtnMore.addEventListener("click", () => {
+
+// });
